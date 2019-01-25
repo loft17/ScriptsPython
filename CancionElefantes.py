37350dbd7c166a5b0e6228a7c89532d73680051b
loft17 <loft17@gmail.com>
2019-01-25 12:54:35 +0100

    Add files via upload

---
+++ CancionElefantes.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+#
+#
+# NAME: 	CancionElefantes
+# VERSION:	1.1.0 
+# --------------------------------------------------------------------
+#
+# email: web@joseromera.net
+# url: https://www.joseromera.net
+# copyright: All right reserved
+#
+# --------------------------------------------------------------------
+#
+
+nElefantes = 1
+nElefantesContar = int(input('Elefantes a contar: '))
+print ('')
+
+
+while nElefantes <= nElefantesContar:
+	print (nElefantes, 'Elefante se balanceaba, sobre la tela de una araña')
+	print ('Como veian que no se caia, fueron a llamar a otor elefante.')
+	print ('')
+
+	nElefantes = nElefantes + 1
+		
